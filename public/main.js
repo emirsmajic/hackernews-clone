3c4e9f63ca3ca4c17a9af6e627dad9aae00d37f8
emirsmajic <emir_smajic@outlook.com>
2020-11-20 21:29:27 +0100

    small redesign

--- public/main.js
+++ public/main.js
@@ -4,10 +4,8 @@ points.forEach((point) => {
   const numberOfPoints = parseInt(point.innerText.split(" ")[0])
   if (!numberOfPoints) point.style.padding = "0px"
   if (numberOfPoints > 150) {
-    point.style.color = "#276749"
-    point.style.background = "#F0FFF4"
+    point.style.color = "#047857"
   } else {
-    point.style.color = "#E53E3E"
-    point.style.background = "#FFF5F5"
+    point.style.color = "#E11D48"
   }
 })
