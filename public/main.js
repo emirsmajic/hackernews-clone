c00c521c19eb0560f4b844f37ea2e9c61cabf36c
emirsmajic <emir_smajic@outlook.com>
2020-11-21 00:21:49 +0100

    span colors change

--- public/main.js
+++ public/main.js
@@ -4,8 +4,12 @@ points.forEach((point) => {
   const numberOfPoints = parseInt(point.innerText.split(" ")[0])
   if (!numberOfPoints) point.style.padding = "0px"
   if (numberOfPoints > 150) {
-    point.style.color = "#047857"
+    point.style.background = "#ECFDF5"
+    point.style.borderColor = "#A7F3D0"
+    point.style.color = "#059669"
   } else {
+    point.style.background = "#FFF1F2"
+    point.style.borderColor = "#FECDD3"
     point.style.color = "#E11D48"
   }
 })
